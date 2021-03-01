3f4595638c700aa8718b97e2a28bf3010ba63983
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-03-01 17:41:19 +0900

    feat: ログ情報取得

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import base64
 import json
+from datetime import timedelta
 
 
 def main(event, context):
-    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')  # type: json
     # JSON形式の文字列を辞書に変換
-    d = json.loads(pubsub_message)
-    print(d)
+    log_data = json.loads(pubsub_message)  # type: dict
+    print(log_data)
 
+    log_name = log_data["logName"]
+    time = (log_name["receiveTimestamp"] + timedelta(hours=9)).strftime("%m/%d %H:%M:%s")
+    log = log_data["textPayload"]
 
 if __name__ == '__main__':
     main("event", "context")
